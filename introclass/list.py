8c8b7dfb82bb8c5ff5c78d67dab6953d9f2b38f9
Solomon <kyobenimaru001@gmail.com>
2024-03-18 08:21:44 +0300

    commit for the class 17-03-2024

---
+++ introclass/list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# students = ['James', 2, 'Emacs']
+
+# Nest lists
+
+students = ['James','Solomon', 'Jane', 'Kyle', 'James']
+
+# Range 3 params (start,last, steps)
+# numbers = list(range(0,100))
+# students.append('Levi')
+students.sort()
+print(students)
+
+
+# print("Hello")
+
