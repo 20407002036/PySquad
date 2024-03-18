3e87e0e05698411e143aa4441971e2708e18facf
Solomon <kyobenimaru001@gmail.com>
2024-03-18 08:59:36 +0300

    commit for the sum of array question

---
+++ Daily_tasks/set.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+number = {2,3,5,7,8,6,44}
+print(number)
--- Daily_tasks/sumOfValuesInList.py
+++ Daily_tasks/sumOfValuesInList.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-"""
-
-"""
-
-
 def sumOfValues(list):
     """
     list of int
@@ -18,16 +13,65 @@ def sumOfValues(list):
     
     Return: sum of 2ndlargest and second smallest int in the list
     """
+    size = len(list)
 
-    list.sort()
-    # num = [1,2,3,4,5,6,7,8]
+    if size == 0:
+        return(0)
+    elif size == 1:
+        return(list[0])
+    else:
+        list.sort()
+        print(list)
+        # num = []
 
-    # print(len(num))
+        # print(len(num))
 
-    a = len(list) -2
+        a = len(list) -2
 
-    return(list[1] + list[a])
+        return(list[1] + list[a])
 
 
-numbers = [2,1,4,5,3,6,8,7]
-print(sumOfValues(numbers))
+def sumFromASet(list):
+    size = len(list)
+
+    if size == 0:
+        return(0)
+    elif size == 1:
+        
+        return(list[0])
+    else:
+        # create a set
+        # iterate through the list appending the set when
+        # new item is encounterd
+
+        numberSet = set()
+        for item in list:
+            if item not in numberSet:
+                numberSet.add(item)
+
+
+
+def sumOfArray(list):
+    """
+    for Python has no built in array. list is commanly used for the same
+
+    to find sum of elements in the Array.
+    Assume that array has only ints
+
+    use the sum function
+    """
+
+    return sum(list)
+    
+numbers = [2,1,4,5,3,6,8,7,8,8]
+# print(sumOfValues(numbers))
+
+# sumFromASet(numbers)
+
+print(sumOfArray(numbers))
+
+age = [21]
+# print(sumOfValues(age))
+
+cash = []
+# print(sumOfValues(cash))
